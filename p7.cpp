90aae31ee1dd989ea54b5f776e4cb5c227eabb82
rishabh <rishabhsachdeva775@gmail.com>
2021-03-19 23:15:24 +0530

    array 8

---
+++ p7.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std; 
+
+int numberOfPaths(int m, int n) 
+{ 
+	if (m == 1 || n == 1) 
+		return 1; 
+
+	return numberOfPaths(m - 1, n) + numberOfPaths(m, n - 1); 
+
+} 
+
+int main() 
+{ 
+	cout << numberOfPaths(3, 3); 
+	return 0; 
+} 
