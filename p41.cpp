b2071c34ba4512342e3c1e5b8344a903256b2baa
rishabh <rishabhsachdeva775@gmail.com>
2021-03-31 20:17:04 +0530

    program

---
+++ p41.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void printRLE(string str)
+{
+	int n = str.length();
+	for (int i = 0; i < n; i++) {
+
+		int count = 1;
+		while (i < n - 1 && str[i] == str[i + 1]) {
+			count++;
+			i++;
+		}
+
+		cout << str[i] << count;
+	}
+}
+
+int main()
+{
+	string str = "wwwwaaadexxxxxxywww";
+	printRLE(str);
+	return 0;
+}
