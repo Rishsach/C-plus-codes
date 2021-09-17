d7c372d4b47b0abaf4714e4a7efcf2e0ce77cfce
rishabh <rishabhsachdeva775@gmail.com>
2021-09-17 19:15:51 +0530

    program

---
+++ p238.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        
+        if (matrix.size() == 0 or matrix[0].size() == 0)
+        {
+            return false;
+        }
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int start = 0;
+        int end = m * n - 1;
+        
+        while(start + 1 < end)
+        {
+            int mid = start + (end - start) / 2;
+            int val = matrix[mid / n][mid % n];
+            
+            if (val > target)
+            {
+                end = mid;
+            }
+            else if (val < target)
+            {
+                start = mid;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        return (matrix[start / n][start % n] == target || matrix[end / n][end % n] == target);
+    }
+};
