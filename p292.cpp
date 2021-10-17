56e227b23ae01f46b9584650ee2f7f840ed1da9d
rishabh <rishabhsachdeva775@gmail.com>
2021-10-17 18:57:17 +0530

    program

--- p292.cpp
+++ p292.cpp
@@ -2,13 +2,13 @@ class Solution {
  public:
   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
     vector<int> ans;
-    deque<int> q;  // max queue
+    deque<int> q;  
 
     for (int i = 0; i < nums.size(); ++i) {
       while (!q.empty() && q.back() < nums[i])
         q.pop_back();
       q.push_back(nums[i]);
-      if (i >= k && nums[i - k] == q.front())  // out of bound
+      if (i >= k && nums[i - k] == q.front())  
         q.pop_front();
       if (i >= k - 1)
         ans.push_back(q.front());
