64ebb62346c5fe952e7fb792c0ac46d0112867c3
rishabh <rishabhsachdeva775@gmail.com>
2021-10-28 02:12:49 +0530

    program

--- p309.cpp
+++ p309.cpp
@@ -5,23 +5,30 @@ class Solution {
       return {};
 
     vector<vector<int>> ans;
-    queue<TreeNode*> q{{root}};
+    deque<TreeNode*> q{{root}};
     bool isLeftToRight = true;
 
     while (!q.empty()) {
-      const int size = q.size();
-      vector<int> currLevel(size);
-      for (int i = 0; i < size; ++i) {
-        TreeNode* node = q.front();
-        q.pop();
-        const int index = isLeftToRight ? i : size - i - 1;
-        currLevel[index] = node->val;
-        if (node->left)
-          q.push(node->left);
-        if (node->right)
-          q.push(node->right);
-      }
-      ans.push_back(node);
+      vector<int> currLevel;
+      for (int size = q.size(); size > 0; --size)
+        if (isLeftToRight) {
+          TreeNode* node = q.front();
+          q.pop_front();
+          currLevel.push_back(node->val);
+          if (node->left)
+            q.push_back(node->left);
+          if (node->right)
+            q.push_back(node->right);
+        } else {
+          TreeNode* node = q.back();
+          q.pop_back();
+          currLevel.push_back(node->val);
+          if (node->right)
+            q.push_front(node->right);
+          if (node->left)
+            q.push_front(node->left);
+        }
+      ans.push_back(currLevel);
       isLeftToRight = !isLeftToRight;
     }
 
