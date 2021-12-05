3ed7734e87e4b5465fe6a4b1b9cd86d13e423189
rishabh <rishabhsachdeva775@gmail.com>
2021-12-05 20:14:33 +0530

    program

---
+++ p354.cpp
@@ -0,0 +1,38 @@
+class RandomizedSet {
+ public:
+  
+  bool insert(int val) {
+    if (valToIndex.count(val))
+      return false;
+
+    valToIndex[val] = vals.size();
+    vals.push_back(val);
+
+    return true;
+  }
+
+  
+  bool remove(int val) {
+    if (!valToIndex.count(val))
+      return false;
+
+    const int index = valToIndex[val];
+    
+    valToIndex[vals.back()] = index;
+    valToIndex.erase(val);
+    vals[index] = vals.back();
+    vals.pop_back();
+
+    return true;
+  }
+
+  
+  int getRandom() {
+    const int index = rand() % vals.size();
+    return vals[index];
+  }
+
+ private:
+  unordered_map<int, int> valToIndex;  
+  vector<int> vals;
+};
