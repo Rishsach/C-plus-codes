46d75acd328eafcd11e77c4adc65c76bdd87c082
rishabh <rishabhsachdeva775@gmail.com>
2021-12-30 23:05:44 +0530

    program

---
+++ p393.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n =  matrix.size();
+        int m =  matrix[0].size();
+        
+        vector<vector<int>> dp(n, vector<int>(m ,0));
+        int maxi = 0;
+        for (int i =0;i<n; i++)  {
+            dp[i][0] = matrix[i][0] - '0';
+            maxi = max(maxi, dp[i][0]);
+        }
+        
+         for (int j =0;j<m; j++) {
+             dp[0][j] = matrix[0][j] - '0';
+                  maxi = max(maxi, dp[0][j]);
+         }
+        
+        
+        for (int i =1; i<n; i++){
+            for (int j =1; j<m; j++){
+                if(matrix[i][j] == '1'){
+                    dp[i][j] =  min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]}) + 1;
+                    
+                    maxi =  max(maxi, dp[i][j] * dp[i][j]);
+                }
+                
+            }
+        }
+        return maxi;
+    }
+};
